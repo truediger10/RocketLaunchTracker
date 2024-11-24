f30008ad2926db7a29b2bb51346caeb75ddd1607
truediger10 <troy.ruediger@gmail.com>
2024-11-24 00:42:55 -0500

    Initial Commit

---
+++ RocketLaunchTracker/RocketLaunchTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketLaunchTrackerApp.swift
+//  RocketLaunchTracker
+//
+//  Created by Troy Ruediger on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct RocketLaunchTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RocketLaunchTrackerTests/RocketLaunchTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  RocketLaunchTrackerTests.swift
+//  RocketLaunchTrackerTests
+//
+//  Created by Troy Ruediger on 11/24/24.
+//
+
+import Testing
+
+struct RocketLaunchTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
